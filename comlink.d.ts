a3361a135ac025bc68421cd0e4ad0d381639f63c
Zixuan Chen <remch183@outlook.com>
2022-03-26 21:38:34 +0800

    chore: refine deno test

--- comlink.d.ts
+++ comlink.d.ts
@@ -3,8 +3,3 @@
         const mod: () => import("comlink").Remote<typeof import("./src/wasmEntry")>
         export default mod
       }
-
-      declare module "comlink:./worker2" {
-        const mod: () => import("comlink").Remote<typeof import("./src/worker2")>
-        export default mod
-      }
--- deno/run.ts
+++ deno/run.ts
@@ -1,4 +1,11 @@
+import {
+  delay,
+  waitFor,
+} from 'https://deno.land/x/mighty_promise@v0.0.10/mod.ts';
+const viteServer = 'http://localhost:3000';
+
 const testFiles = [] as string[];
+
 for await (const file of Deno.readDir('./deno')) {
   if (file.name.endsWith('test.ts')) {
     testFiles.push(file.name);
@@ -11,23 +18,38 @@ if (testFiles.length === 0) {
 
 console.log('Core Number', navigator.hardwareConcurrency);
 console.log();
+
+let serverProcess;
+if (!(await isServerAvailable())) {
+  serverProcess = Deno.run({ cmd: ['pnpm', 'start'], stdout: 'null' });
+  console.log('Starting server...');
+  await waitFor({
+    condition: isServerAvailable,
+    timeout: 20_000,
+  });
+  await delay(1000);
+  console.log('Starting server done\n');
+}
+
 console.log('Start testing...');
 for (const file of testFiles) {
-  const path = 'http://localhost:3000/deno/' + file;
+  const path = viteServer + '/deno/' + file;
   console.log('-----------------------------\n');
   console.log('>> ' + path + ':\n\n');
   Deno.run;
   await Deno.run({
-    cmd: [
-      'deno',
-      'test',
-      '-Aq',
-      '--location',
-      'http://localhost:3000/',
-      path,
-      '--reload',
-    ],
+    cmd: ['deno', 'test', '-Aq', '--location', viteServer, path, '--reload'],
     stdout: 'inherit',
   }).status();
   console.log();
+}
+
+const promise = serverProcess?.status();
+serverProcess?.kill('SIGTERM');
+serverProcess?.close();
+await promise;
+
+async function isServerAvailable() {
+  const res = await fetch(viteServer);
+  return res.status === 200;
 }
