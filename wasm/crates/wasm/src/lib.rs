df7d260cd02472866f24d631427875e5871609e9
Zixuan Chen <remch183@outlook.com>
2022-05-10 20:24:59 +0800

    fix: type error

--- wasm/crates/wasm/src/lib.rs
+++ wasm/crates/wasm/src/lib.rs
@@ -36,15 +36,16 @@ impl TreeData for Data {
 
 #[wasm_bindgen]
 pub fn build_a_tree(input: &[f64], target: &[f64]) -> usize {
+    let herd = Herd::new();
     let mut nodes = vec![];
+    let mem = herd.get();
     for i in (0..input.len()).step_by(2) {
-        nodes.push(Node::<'_, f64, 2, 4, Data>::new_point(
+        nodes.push(mem.alloc(Node::<'_, f64, 2, 4, Data>::new_point(
             [input[i], input[i + 1]],
             i / 2,
-        ));
+        )));
     }
 
-    let herd = Herd::new();
     let tree = generic_tree::GenericTree::<'_, f64, 2, 4, Data>::new_in_par(&herd, nodes, 0.1, 3);
     // let tree = generic_tree::GenericTree::<f64, 2, usize>::new_in_par(nodes, 0.1, 10);
     let data = *tree.find_closest(&[target[0], target[1]]).unwrap().data();
